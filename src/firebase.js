82518e8105125bcf7fbed0aaa849eacd01a5d875
justinWaves <juhstinn@gmail.com>
2022-11-02 16:49:19 -0700

    init commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCFEgvf99VLAECE7IbIaXDhL0yTYuXK9Wc",
+  authDomain: "netflix-clone-38dd5.firebaseapp.com",
+  projectId: "netflix-clone-38dd5",
+  storageBucket: "netflix-clone-38dd5.appspot.com",
+  messagingSenderId: "355899310811",
+  appId: "1:355899310811:web:20cd6f6fcbad95898202fe",
+};
