bcf015567b0f86d3fa896e5e60ca04978f0922a3
justinWaves <juhstinn@gmail.com>
2022-11-06 12:48:51 -0800

    sign up screen built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React from "react";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./Screens/HomeScreen";
+import Login from "./Screens/LoginScreen";
 import "./App.css";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <HomeScreen />,
+//   },
+//   {
+//     path: "test",
+//     element: (
+//       <div>
+//         <h1>Hello World</h1>
+//       </div>
+//     ),
+//   },
+// ]);
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <BrowserRouter>
+        {!user ? (
+          <Login />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="test" element={<h1>Yayyyy</h1>} />
+          </Routes>
+        )}
+      </BrowserRouter>
     </div>
   );
 }
